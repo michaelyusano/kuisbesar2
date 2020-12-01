f3f89d3e3e573fa4e1e999d09d77c829d1087ff5
michaelyusano <61056211+michaelyusano@users.noreply.github.com>
2020-12-01 12:01:55 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function borrow(){
+  Swal.fire(
+   'Request peminjaman berhasil!',
+   'Mohon membawa buku ke admin perpustakaan',
+   'success'
+   )
+}
+
+function reqreturn(){
+  Swal.fire(
+   'Request pengembalian berhasil!',
+   'Mohon membawa buku ke admin perpustakaan',
+   'success'
+   )
+}
+
+function update(){
+  Swal.fire(
+    'Profil berhasil diperbarui!',
+    '',
+    'success'
+    )
+}
